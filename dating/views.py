5df8303e14c6ec149ec04283587cab2182622391
JishnuRameshC <jishnurameshc@gmail.com>
2024-08-19 09:50:42 +0530

    update some pages views & url

--- dating/views.py
+++ dating/views.py
@@ -6,7 +6,52 @@ from django.views.generic import TemplateView
 def TestView(request):
     return render(request, 'index.html')
 
-# flowclass 
+
+# SignUp
+
+class SignupView(TemplateView):
+    template_name='account/signup.html'
+
+
+class LoginView(TemplateView):
+    template_name='account/login.html'
+
+
+class ProfessionView(TemplateView):
+    template_name='account/profession.html'
+
+
+class Rel_GoalView(TemplateView):
+    template_name='account/relationship_goal.html'
+
+
+class InterestView(TemplateView):
+    template_name='account/interested.html'
+
+
+# Group 
+class CreateGroupView(TemplateView):
+    template_name='groups/create_group.html'
+
+
+class GroupListView(TemplateView):
+    template_name = 'groups/groups.html'
+
+
+# SubscriptionPlan
+class SubscriptionPlanView(TemplateView):
+    template_name = 'payment/subscription_plans.html'
+
+
+class PaymentMethodsView(TemplateView):
+    template_name = 'payment/payment_methods.html'
+
+
+class AddPaymentMethodsView(TemplateView):
+    template_name = 'payment/add_payment.html'
+
+# Dating 
+
 class FirstView(TemplateView):
     template_name = 'test.html'
 
@@ -35,36 +80,42 @@ class Page6View(TemplateView):
 class Page7View(TemplateView):
     template_name = 'Dating/page7.html'
 
+# UserProfileFlow
+class ChangePasswordView(TemplateView):
+    template_name = 'User_profile_flow\change_password.html'
 
 
-# SignUp
+class ContactedView(TemplateView):
+    template_name = 'User_profile_flow\contacted.html'
 
-# class SignupView(TemplateView):
-#     template_name='account/signup.html'
 
-# class LoginView(TemplateView):
-#     template_name='account/login.html'
-    
-# class ProfessionView(TemplateView):
-#     template_name='account/profession.html'
-    
-# class Rel_GoalView(TemplateView):
-#     template_name='account/relationship_goal.html'
-    
-# class InterestView(TemplateView):
-#     template_name='account/interested.html'
-    
-# class CreateGroupView(TemplateView):
-#     template_name='groups/create_group.html'
-    
-# class SubscriptionPlanView(TemplateView):
-#     template_name = 'payment/subscription_plans.html'
+class EditMyProfileView(TemplateView):  
+    template_name = 'User_profile_flow\edit_profile.html'
 
-# class PaymentMethodsView(TemplateView):
-#     template_name = 'payment/payment_methods.html'
+class MessageView(TemplateView):    
+    template_name = 'User_profile_flow\message.html'
 
-# class AddPaymentMethodsView(TemplateView):
-#     template_name = 'payment/add_payment.html'
-    
-# class GroupListView(TemplateView):
-#     template_name = 'groups/groups.html'
+
+class SettingsView(TemplateView):   
+    template_name = 'User_profile_flow\settings.html'
+
+
+class StoryView(TemplateView):    
+    template_name = 'User_profile_flow\story.html'
+
+
+class UpdateViewProfile(TemplateView):    
+    template_name = 'User_profile_flow\update_profile.html'
+
+
+class UpdateViewStory(TemplateView):    
+    template_name = 'User_profile_flow\update_story.html'   
+
+
+class UserProfile(TemplateView):    
+    template_name = 'User_profile_flow\user_profile.html'
+
+
+class ShortlistedView(TemplateView):    
+    template_name = 'User_profile_flow\shortlist.html'
+
